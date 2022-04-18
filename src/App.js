e76df648ed7b3af674075d3b373db7724c921761
icitwd <ilhan.c.ilhan@gamil.com>
2022-04-18 13:31:07 +0300

    updating our app with a shop page, collection preview component and collection item component. ALSO ADDED ROUTES.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Route, Routes } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
+function HatsPage() {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+}
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
     </div>
   );
 }
